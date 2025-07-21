615e1dee781f78e243dc3d8722837084cdfa8e48
Mikey Appuhn <mike.appuhn@gmail.com>
2025-07-21 15:55:09 -0700

    styling changes mostly

---
+++ components/navbar.jsx
@@ -0,0 +1,14 @@
+
+export default function Nav () {
+    return (
+          <header className="sticky top-0 flex gap-[12px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="/"
+          rel="noopener noreferrer"
+        >
+          Home
+        </a>
+      </header>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,14 @@
 import Image from "next/image";
 import { instrument, spaceGrotesk } from "@/styles/font";
+import Nav from "@/components/navbar";
 
 export default function Home() {
   return (
     <div
       className={`font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
     >
-      <header className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="/"
-          rel="noopener noreferrer"
-        >
-          Home
-        </a>
-      </header>
 
-
+      <Nav />
       <main 
           className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start"
       >
