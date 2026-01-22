9636e395ae45cf6ddfc0164d1c7f8607e88b3531
Mikey Appuhn <mike.appuhn@gmail.com>
2026-01-22 13:12:14 -0800

    feat: added GA tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import "@/styles/globals.scss";
 import "@/styles/pages/index.scss";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 export default function App({ Component, pageProps }) {
   return (
     <main className="font-sans">
       <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-T2KW340RY4" />
     </main>
   );
 }
